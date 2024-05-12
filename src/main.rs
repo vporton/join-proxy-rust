8b6cb00242e29b75f14fb58a794573f6460ff9a7
Victor Porton <porton.victor@gmail.com>
2024-05-12 12:20:11 +0300

    serialize_http_response()

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-use actix_web::http::header::{HeaderName, HeaderValue};
+// Two similar functions with different data types follow:
 
 fn serialize_http_request(request: actix_web::HttpRequest, bytes: actix_web::web::Bytes) -> anyhow::Result<Vec<u8>> {
     let headers_list = request.headers().into_iter()
-        .map(|(k, v): (&HeaderName, &HeaderValue)| -> anyhow::Result<String> {
+        .map(|(k, v)| -> anyhow::Result<String> {
             Ok(k.to_string() + "\t" + v.to_str()?)
         })
         .collect::<anyhow::Result<Vec<_>>>()?;
     let headers_joined = headers_list.into_iter().reduce(|a, b| a + "\r" + &b);
     let headers_joined = headers_joined.unwrap_or_else(|| "".to_string());
-    let header_part = request.method().as_str().to_owned() + "\n" + &request.uri().to_string() + "\n" + &headers_joined + "\n";
+    let header_part = request.method().as_str().to_owned() + "\n" + &request.uri().to_string() + "\n" + &headers_joined;
 
-    Ok([header_part.as_bytes(), bytes.to_vec().as_slice()].concat())
+    Ok([header_part.as_bytes(), b"\n", bytes.to_vec().as_slice()].concat())
+}
+
+fn serialize_http_response(response: reqwest::Response, bytes: bytes::Bytes) -> anyhow::Result<Vec<u8>> {
+    let headers_list = response.headers().into_iter()
+        .map(|(k, v)| -> anyhow::Result<String> {
+            Ok(k.to_string() + "\t" + v.to_str()?)
+        })
+        .collect::<anyhow::Result<Vec<_>>>()?;
+    let headers_joined = headers_list.into_iter().reduce(|a, b| a + "\r" + &b);
+    let headers_joined = headers_joined.unwrap_or_else(|| "".to_string());
+    let header_part = response.status().to_string() + "\n" + response.url().as_str() + "\n" + &headers_joined;
+
+    Ok([header_part.as_bytes(), b"\n", &bytes].concat())
 }
 
 #[actix::main]
