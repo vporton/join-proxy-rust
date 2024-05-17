33988ecfa232bab2e5580a69747d2d8470af4a46
Victor Porton <porton.victor@gmail.com>
2024-05-18 02:42:42 +0300

    simplify errors

--- proxy/src/errors.rs
+++ proxy/src/errors.rs
@@ -3,45 +3,59 @@ use http_for_actix::status::InvalidStatusCode;
 use k256::ecdsa;
 use thiserror::Error;
 use std::fmt::{Debug, Display, Formatter};
+use derive_more::From;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, From)]
 pub enum MyError {
+    #[error("{0}")]
     Anyhow(Box<anyhow::Error>),
+    #[error("{0}")]
     Io(std::io::Error),
+    #[error("Invalid HTTP method")]
     InvalidMethod(http::method::InvalidMethod),
+    #[error("Invalid HTTP response")]
     HttpResponse(reqwest::Response),
+    #[error("Request error: {0}")]
     ReqwestError(reqwest::Error),
+    #[error("Invalid HTTP status code")]
     InvalidStatus(InvalidStatusCode),
+    #[error("Invalid HTTP header")]
     HeaderToStrForActix(http_for_actix::header::ToStrError),
+    #[error("Invalid HTTP header")]
     HeaderToStr(http::header::ToStrError),
     // InvalidHeaderName(Box<http_for_actix::header::IntoHeaderName>),
+    #[error("The DB is corrupted")]
     MyCorruptedDB(MyCorruptedDBError),
+    #[error("Invalid HTTP header name")]
     InvalidHeaderName(InvalidHeaderNameError),
+    #[error("Invalid HTTP header value")]
     InvalidHeaderValue(InvalidHeaderValueError),
+    #[error("Invalid Base64 encoded data")]
     Base64Decode(base64::DecodeError),
+    #[error("ECDSA error: {0}")]
     Ecdsa(ecdsa::Error),
 }
 
-impl Display for MyError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Anyhow(e) => Debug::fmt(&*e, f),
-            Self::Io(e) => Debug::fmt(&*e, f),
-            Self::InvalidMethod(e) => Debug::fmt(&*e, f),
-            Self::HttpResponse(e) => Debug::fmt(&*e, f),
-            Self::ReqwestError(e) => Debug::fmt(&*e, f),
-            Self::InvalidStatus(e) => Debug::fmt(&*e, f),
-            Self::HeaderToStrForActix(e) => Debug::fmt(&*e, f),
-            Self::HeaderToStr(e) => Debug::fmt(&*e, f),
-            // Self::InvalidHeaderName(e) => Debug::fmt(&*e, f),
-            Self::MyCorruptedDB(e) => Debug::fmt(&*e, f),
-            Self::InvalidHeaderName(e) => Debug::fmt(&*e, f),
-            Self::InvalidHeaderValue(e) => Debug::fmt(&*e, f),
-            Self::Base64Decode(e) => Debug::fmt(&*e, f),
-            Self::Ecdsa(e) => Debug::fmt(&*e, f),
-        }
-    }
-}
+// impl Display for MyError {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             Self::Anyhow(e) => Debug::fmt(&*e, f),
+//             Self::Io(e) => Debug::fmt(&*e, f),
+//             Self::InvalidMethod(e) => Debug::fmt(&*e, f),
+//             Self::HttpResponse(e) => Debug::fmt(&*e, f),
+//             Self::ReqwestError(e) => Debug::fmt(&*e, f),
+//             Self::InvalidStatus(e) => Debug::fmt(&*e, f),
+//             Self::HeaderToStrForActix(e) => Debug::fmt(&*e, f),
+//             Self::HeaderToStr(e) => Debug::fmt(&*e, f),
+//             // Self::InvalidHeaderName(e) => Debug::fmt(&*e, f),
+//             Self::MyCorruptedDB(e) => Debug::fmt(&*e, f),
+//             Self::InvalidHeaderName(e) => Debug::fmt(&*e, f),
+//             Self::InvalidHeaderValue(e) => Debug::fmt(&*e, f),
+//             Self::Base64Decode(e) => Debug::fmt(&*e, f),
+//             Self::Ecdsa(e) => Debug::fmt(&*e, f),
+//         }
+//     }
+// }
 
 #[derive(Debug, Default, Error)]
 pub struct MyCorruptedDBError {}
@@ -73,84 +87,6 @@ impl Display for InvalidHeaderValueError {
 impl From<anyhow::Error> for MyError {
     fn from(err: anyhow::Error) -> Self {
         Self::Anyhow(Box::new(err))
-    }
-}
-
-impl From<std::io::Error> for MyError {
-    fn from(err: std::io::Error) -> Self {
-        Self::Io(err)
-    }
-}
-
-impl From<http::method::InvalidMethod> for MyError {
-    fn from(err: http::method::InvalidMethod) -> Self {
-        Self::InvalidMethod(err)
-    }
-}
-
-impl From<reqwest::Response> for MyError {
-    fn from(err: reqwest::Response) -> Self {
-        Self::HttpResponse(err)
-    }
-}
-
-impl From<reqwest::Error> for MyError {
-    fn from(err: reqwest::Error) -> Self {
-        Self::ReqwestError(err)
-    }
-}
-
-impl From<InvalidStatusCode> for MyError {
-    fn from(err: InvalidStatusCode) -> Self {
-        Self::InvalidStatus(err)
-    }
-}
-
-impl From<http_for_actix::header::ToStrError> for MyError {
-    fn from(err: http_for_actix::header::ToStrError) -> Self {
-        Self::HeaderToStrForActix(err)
-    }
-}
-
-impl From<http::header::ToStrError> for MyError {
-    fn from(err: http::header::ToStrError) -> Self {
-        Self::HeaderToStr(err)
-    }
-}
-
-// impl From<Box<http_for_actix::header::IntoHeaderName>> for MyError {
-//     fn from(err: Box<http_for_actix::header::IntoHeaderName>) -> Self {
-//         Self::InvalidHeaderName(err)
-//     }
-// }
-
-impl From<MyCorruptedDBError> for MyError {
-    fn from(err: MyCorruptedDBError) -> Self {
-        Self::MyCorruptedDB(err)
-    }
-}
-
-impl From<InvalidHeaderNameError> for MyError {
-    fn from(err: InvalidHeaderNameError) -> Self {
-        Self::InvalidHeaderName(err)
-    }
-}
-
-impl From<InvalidHeaderValueError> for MyError {
-    fn from(err: InvalidHeaderValueError) -> Self {
-        Self::InvalidHeaderValue(err)
-    }
-}
-
-impl From<base64::DecodeError> for MyError {
-    fn from(err: base64::DecodeError) -> Self {
-        Self::Base64Decode(err)
-    }
-}
-
-impl From<ecdsa::Error> for MyError {
-    fn from(err: ecdsa::Error) -> Self {
-        Self::Ecdsa(err)
     }
 }
 
