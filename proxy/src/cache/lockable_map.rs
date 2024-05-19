6f97925efbaf51b334e7d78eabe09df8b4dd7bdd
Victor Porton <porton.victor@gmail.com>
2024-05-19 21:31:43 +0300

    lockable_map.rs compiled again

--- proxy/src/cache/lockable_map.rs
+++ proxy/src/cache/lockable_map.rs
@@ -2,13 +2,15 @@ use std::collections::HashMap;
 use std::ops::{Deref, DerefMut};
 use std::sync::{LockResult, TryLockResult};
 
+use tokio::sync::TryLockError;
+
 // FIXME: use async mutex.
 
-trait MutexGuard<'a, T: ?Sized + 'a>: Deref<Target = T> /*+ DerefMut<Target = T>*/ {
+trait MutexGuard<T: ?Sized>: Deref<Target = T> /*+ DerefMut<Target = T>*/ {
     fn set(&mut self, value: T);
 }
 
-impl<'a, T: ?Sized + 'a> MutexGuard<'a, T> for std::sync::MutexGuard<'a, T> {
+impl<T: ?Sized> MutexGuard<T> for tokio::sync::MutexGuard<'_, T> {
     fn set(&mut self, value: T) {
         *self.deref_mut() = value;
     }
@@ -17,34 +19,34 @@ impl<'a, T: ?Sized + 'a> MutexGuard<'a, T> for std::sync::MutexGuard<'a, T> {
 // TODO: more abstract error handling
 trait Mutex<T: ?Sized> {
     // fn get_mut(&mut self) -> LockResult<&mut T>; // not available for Redis
-    fn into_inner(self) -> LockResult<T>
+    fn into_inner(self) -> T
         where
             T: Sized;
-    fn lock(&self) -> LockResult<impl MutexGuard<'_, T>>;
-    fn try_lock(&self) -> TryLockResult<impl MutexGuard<'_, T>>;
+    async fn lock(&self) -> impl MutexGuard<T>;
+    fn try_lock(&self) -> Result<impl MutexGuard<T>, TryLockError>;
 }
 
-impl<T: ?Sized> Mutex<T> for std::sync::Mutex<T> {
-    fn into_inner(self) -> LockResult<T>
+impl<T: ?Sized> Mutex<T> for tokio::sync::Mutex<T> {
+    fn into_inner(self) -> T
         where
             T: Sized
     {
         self.into_inner()
     }
 
-    fn lock(&self) -> LockResult<impl MutexGuard<'_, T>> {
-        self.lock()
+    async fn lock(&self) -> impl MutexGuard<T> {
+        self.lock().await
     }
 
-    fn try_lock(&self) -> TryLockResult<impl MutexGuard<'_, T>> {
+    fn try_lock(&self) -> Result<impl MutexGuard<T>, TryLockError> {
         self.try_lock()
     }
 }
 
-trait AbstractLockableMap<'a, K, V> {
-    type Guard: MutexGuard<'a, Option<V>>;
+trait AbstractLockableMap<K, V> {
+    type Guard<'a>: MutexGuard<Option<V>> where Self: 'a;
 
-    fn lock(&mut self, key: K) -> Self::Guard;
+    async fn lock(&mut self, key: K) -> Self::Guard<'_>;
 
     // fn insert(&mut self, key: K, value: V);
 
@@ -54,7 +56,7 @@ trait AbstractLockableMap<'a, K, V> {
 }
 
 struct LockableMap<K, V> {
-    map: HashMap<K, std::sync::Mutex<Option<V>>>,
+    map: HashMap<K, tokio::sync::Mutex<Option<V>>>,
 }
 
 impl<K, V> LockableMap<K, V> {
@@ -64,18 +66,17 @@ impl<K, V> LockableMap<K, V> {
 }
 
 // Code based on https://g.co/gemini/share/5045754c1381
-impl<'a, K, V> AbstractLockableMap<'a, K, V> for LockableMap<K, V> 
+impl<K, V> AbstractLockableMap<K, V> for LockableMap<K, V> 
 where
     K: std::hash::Hash + Eq + Clone, // TODO: Is `Clone` needed?
-    V: 'a
 {
-    type Guard = std::sync::MutexGuard<'a, Option<V>>;
+    type Guard<'a> = tokio::sync::MutexGuard<'a, Option<V>> where K: 'a, V: 'a;
 
-    fn lock(&mut self, key: K) -> std::sync::MutexGuard<'a, Option<V>> {
+    async fn lock(&mut self, key: K) -> tokio::sync::MutexGuard<Option<V>> {
         self.map.entry(key.clone())
-            .or_insert_with(|| Mutex::new(None))
+            .or_insert_with(|| tokio::sync::Mutex::new(None))
             .lock()
-            .unwrap()
+            .await
     }
 
     // fn insert(&mut self, key: K, value: V) {
