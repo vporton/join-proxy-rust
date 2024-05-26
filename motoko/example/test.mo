117eb5b4f30b9a2b3fb0d9278c336c57590fd397
Victor Porton <porton.victor@gmail.com>
2024-05-26 22:37:28 +0300

    misc e2e

--- motoko/example/test.mo
+++ motoko/example/test.mo
@@ -1,15 +1,22 @@
 import Call "canister:call";
+import Blob "mo:base/Blob";
 import Bool "mo:base/Bool";
+import Debug "mo:base/Debug";
+import Text "mo:base/Text";
 
 actor Test {
-    public shared func test(addHost: Bool) {
-        await Call.callHttp({
+    public shared func test(addHost: Bool): async Text {
+        let res = await Call.callHttp({
             url = "http://localhost:8081/";
             max_response_bytes = ?10_000;
             headers = if (addHost) [{name = "Host"; value="localhost:8081"}] else [];
             body = null;
             method = #get;
             transform = null;
-        })
+        });
+        let ?body = Text.decodeUtf8(Blob.fromArray(res.body)) else {
+            Debug.trap("No response body.")
+        };
+        body;
     }
 };
--- test/e2e/src/main.rs
+++ test/e2e/src/main.rs
@@ -1,23 +1,28 @@
-use std::path::Path;
+use std::process::Command;
 
-use like_shell::temp_dir_from_template;
-use tempdir::TempDir;
+use like_shell::run_successful_command;
 
-struct Test {
-    dir: TempDir,
-}
+// use like_shell::temp_dir_from_template;
+// use tempdir::TempDir;
 
-impl Test {
-    pub fn new(tmpl_dir: &Path) -> Result<Self, Box<dyn std::error::Error>> {
-        Ok(Self {
-            dir: temp_dir_from_template(tmpl_dir)?,
-        })
-    }
-}
+// struct Test {
+//     // dir: TempDir,
+// }
+
+// impl Test {
+//     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+//         Ok(Self {
+//             // dir: temp_dir_from_template(tmpl_dir)?,
+//         })
+//     }
+// }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tmpl_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("tmpls");
-    let test = Test::new(&tmpl_dir)?;
-    println!("{:?}", &test.dir);
+    // let cargo_manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    // let workspace_dir = cargo_manifest_dir.join("..").join("..");
+    // let tmpl_dir = .join("tmpls");
+    // let test = Test::new()?;
+    run_successful_command(Command::new("dfx").args(["deploy"]))?;
+    // TODO
     Ok(())
 }
