0951ae268a1dfecc5ce5fa848c60ef450351359e
Victor Porton <porton.victor@gmail.com>
2024-05-27 14:00:52 +0300

    testing changes

--- test/e2e/src/main.rs
+++ test/e2e/src/main.rs
@@ -1,7 +1,8 @@
 use std::process::Command;
 
-use ic_agent::Agent;
+use ic_agent::{export::Principal, Agent};
 use like_shell::run_successful_command;
+use dotenv::{dotenv, var};
 
 // use like_shell::temp_dir_from_template;
 // use tempdir::TempDir;
@@ -25,10 +26,15 @@ impl Test {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let cargo_manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    dotenv().ok();
+
+    let test_canister_id = Principal::from_text(var("CANISTER_ID_TEST")?)?;
+
     // let workspace_dir = cargo_manifest_dir.join("..").join("..");
     // let tmpl_dir = .join("tmpls");
     let test = Test::new().await?;
     run_successful_command(Command::new("dfx").args(["deploy"]))?;
+    let res = test.agent.update(&test_canister_id, "test").with_arg(Encode!(false)).call_and_wait().await;
     // TODO
     Ok(())
 }
