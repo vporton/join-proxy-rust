44931980720b25896609f0ce50d72c56badca498
Victor Porton <porton.victor@gmail.com>
2024-05-28 18:29:49 +0300

    fixing bugs in E2E tests

--- test/e2e/src/main.rs
+++ test/e2e/src/main.rs
@@ -1,14 +1,15 @@
-use std::{env::var, fs::{read_to_string, write}, path::{Path, PathBuf}, process::Command, time::Duration};
+use std::{fs::{read_to_string, write, File}, path::{Path, PathBuf}, process::Command, time::Duration};
 
 use candid::{Decode, Encode};
 use fs_extra::{dir, file};
 use ic_agent::{export::Principal, Agent};
 use like_shell::{run_successful_command, temp_dir_from_template, Capture, TemporaryChild};
-use dotenv::dotenv;
+// use dotenv::dotenv;
 use tempdir::TempDir;
 use tokio::time::sleep;
 use toml_edit::{value, DocumentMut};
-use anyhow::{Context, anyhow};
+use anyhow::Context;
+use serde_json::Value;
 
 struct Test {
     dir: TempDir,
@@ -23,44 +24,54 @@ impl Test {
         let cargo_manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
         let workspace_dir = cargo_manifest_dir.join("..").join("..");
         let dir = temp_dir_from_template(tmpl_dir)?;
-        dir::copy(workspace_dir.join("motoko"), dir.path(), &dir::CopyOptions::new())?; // FIXME: What should be `copy_inside` value?
+        dir::copy(workspace_dir.join("motoko"), dir.path(), &dir::CopyOptions::new())
+            .context("Copying files.")?; // FIXME: What should be `copy_inside` value?
         file::copy(
             workspace_dir.join("mops.toml"),
             dir.path().join("mops.toml"),
             &file::CopyOptions::new(),
-        )?;
-        dotenv().ok();
+        ).context("Copying a file.")?;
     
         // TODO: Specifying a specific port is a hack.
         let _dfx_daemon = TemporaryChild::spawn(&mut Command::new(
             "dfx"
-        ).arg("start").current_dir(dir.path()), Capture { stdout: None, stderr: None })
-            .with_context(|| anyhow!("Starting DFX"))?;
+        ).args(["start", "--host", "127.0.0.1:8007"]).current_dir(dir.path()), Capture { stdout: None, stderr: None })
+            .context("Starting DFX")?;
         sleep(Duration::from_millis(1000)).await; // Wait till daemons start.
         run_successful_command(Command::new("mops").arg("install").current_dir(dir.path()))
-            .with_context(|| anyhow!("Installing MOPS packages."))?;
+            .context("Installing MOPS packages.")?;
         run_successful_command(Command::new("dfx").arg("deploy").current_dir(dir.path()))
-            .with_context(|| anyhow!("Deploying."))?;
+            .context("Deploying.")?;
+        // dotenv().ok();
+
         let port_str = read_to_string(
             dir.path().join(".dfx").join("network").join("local").join("webserver-port"),
-        ).with_context(|| anyhow!("Reading port."))?;
-        let port: u16 = port_str.parse()?;
+        ).context("Reading port.")?;
+        let port: u16 = port_str.parse().context("Parsing port number.")?;
 
+        let canister_ids: Value = {
+            let path = dir.path().join(".dfx").join("local").join("canister_ids.json");
+            let file = File::open(path).with_context(|| format!("Opening canister_ids.json"))?;
+            serde_json::from_reader(file).expect("Error parsing JSON")
+        };
+        let test_canister_id = canister_ids.as_object().unwrap()["test"].as_object().unwrap()["local"].as_str().unwrap();
+
+        println!("Connecting to port {port}");
         let res = Self {
             dir,
-            agent: Agent::builder().with_url(format!("http://localhost:{port}")).build()?,
-            test_canister_id: Principal::from_text(var("CANISTER_ID_TEST")?)?,
+            agent: Agent::builder().with_url(format!("http://localhost:{port}")).build().context("Creating Agent")?,
+            test_canister_id: Principal::from_text(test_canister_id)
+                .context("Parsing principal")?,
             // cargo_manifest_dir: cargo_manifest_dir.to_path_buf(),
             workspace_dir: workspace_dir,
         };
-
-        res.agent.fetch_root_key().await?; // DON'T USE this on mainnet
+        res.agent.fetch_root_key().await.context("Fetching root keys.")?; // DON'T USE this on mainnet
 
         let toml_path = res.dir.path().join("config.toml");
-        let toml = read_to_string(&toml_path).context(anyhow!("Reading config."))?;
-        let mut doc = toml.parse::<DocumentMut>().expect("invalid doc");
-        doc["callback"]["canister"] = value(var("CANISTER_ID_TEST")?);
-        write(&toml_path, doc.to_string())?;
+        let toml = read_to_string(&toml_path).context("Reading config.")?;
+        let mut doc = toml.parse::<DocumentMut>().context("Invalid TOML")?;
+        doc["callback"]["canister"] = value(res.test_canister_id.to_string());
+        write(&toml_path, doc.to_string()).context("Writing modified config.")?;
 
         Ok(res)
     }
@@ -68,10 +79,10 @@ impl Test {
 
 async fn test_calls(test: &Test) -> Result<(), Box<dyn std::error::Error>> {
     for add_host in [false, true] {
-        let res =
-            test.agent.update(&test.test_canister_id, "test").with_arg(Encode!(&add_host)?)
+        let res = // TODO
+            test.agent.update(&test.test_canister_id, "test").with_arg(Encode!(&add_host).unwrap())
                 .call_and_wait().await.context("Back-call to IC.")?;
-        assert_eq!(Decode!(&res, String)?, "Test");
+        assert_eq!(Decode!(&res, String).context("Decoding test call response.")?, "Test");
         // TODO: Check two parallel requests.
     }
     Ok(())
@@ -85,13 +96,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let test = Test::new(&tmpl_dir).await?;
     let _test_http = TemporaryChild::spawn(&mut Command::new(
         test.workspace_dir.join("target").join("debug").join("test-server")
-    ), Capture { stdout: None, stderr: None });
+    ), Capture { stdout: None, stderr: None })?;
     let _proxy = TemporaryChild::spawn(&mut Command::new(
         test.workspace_dir.join("target").join("debug").join("joining-proxy")
-    ), Capture { stdout: None, stderr: None });
-    let _test_server = TemporaryChild::spawn(&mut Command::new(
-        test.workspace_dir.join("target").join("debug").join("test-server")
-    ), Capture { stdout: None, stderr: None });
+    ), Capture { stdout: None, stderr: None })?;
     sleep(Duration::from_millis(1000)).await; // Wait till daemons start.
     test_calls(&test).await?;
     // TODO
