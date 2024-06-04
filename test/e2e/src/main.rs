9ebb67d0b94700744a19749638efeeb8d5c34e2c
Victor Porton <porton.victor@gmail.com>
2024-06-05 01:28:38 +0300

    test code reorganized (bug fix)

--- test/e2e/src/main.rs
+++ test/e2e/src/main.rs
@@ -14,10 +14,6 @@ struct Test {
     dir: TempDir,
     // cargo_manifest_dir: PathBuf,
     workspace_dir: PathBuf,
-    agent: Agent,
-    test_canister_id: Principal,
-    call_canister_id: Principal,
-    _dfx_daemon: TemporaryChild,
 }
 
 impl Test {
@@ -25,56 +21,83 @@ impl Test {
         let cargo_manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
         let workspace_dir = cargo_manifest_dir.join("..").join("..");
         let dir = temp_dir_from_template(tmpl_dir)?;
-    
+
+        let res = Self {
+            dir,
+            // cargo_manifest_dir: cargo_manifest_dir.to_path_buf(),
+            workspace_dir: workspace_dir,
+        };
+
+        Ok(res)
+    }
+}
+
+// TODO: Should have more abstract DFXDir.
+struct OurDFX {
+    pub base: Test,
+    test_canister_id: Principal,
+    agent: Agent,
+}
+
+impl OurDFX {
+    pub async fn new(tmpl_dir: &Path) -> Result<Self, Box<dyn std::error::Error>> {
+        let base = Test::new(tmpl_dir).await?;
+
         // TODO: Specifying a specific port is a hack.
-        let dfx_daemon = TemporaryChild::spawn(&mut Command::new(
+        run_successful_command(&mut Command::new(
             "/root/.local/share/dfx/bin/dfx" // TODO: Split path.
-        ).args(["start", "--clean", "--host", "127.0.0.1:8007"]).current_dir(dir.path()), Capture { stdout: None, stderr: None })
+        ).args(["start", "--host", "127.0.0.1:8007", "--background"]).current_dir(base.dir.path()))
             .context("Starting DFX")?;
-        sleep(Duration::from_millis(1000)).await; // Wait till daemons start.
+
+        let port_str = read_to_string(
+            base.dir.path().join(".dfx").join("network").join("local").join("webserver-port"),
+        ).context("Reading port.")?;
+        let port: u16 = port_str.parse().context("Parsing port number.")?;
+
+        println!("Connecting to DFX (port {port})");
         run_successful_command(Command::new(
             "/root/.local/share/dfx/bin/dfx" // TODO: Split path.
         ).args(["deploy"]))?;
         // dotenv().ok();
 
-        let port_str = read_to_string(
-            dir.path().join(".dfx").join("network").join("local").join("webserver-port"),
-        ).context("Reading port.")?;
-        let port: u16 = port_str.parse().context("Parsing port number.")?;
-
         let canister_ids: Value = {
-            let path = dir.path().join(".dfx").join("local").join("canister_ids.json");
+            let path = base.dir.path().join(".dfx").join("local").join("canister_ids.json");
             let file = File::open(path).with_context(|| format!("Opening canister_ids.json"))?;
             serde_json::from_reader(file).expect("Error parsing JSON")
         };
         let test_canister_id = canister_ids.as_object().unwrap()["test"].as_object().unwrap()["local"].as_str().unwrap();
         let call_canister_id = canister_ids.as_object().unwrap()["call"].as_object().unwrap()["local"].as_str().unwrap();
 
-        println!("Connecting to DFX (port {port})");
-        let res = Self {
-            dir,
-            _dfx_daemon: dfx_daemon,
-            agent: Agent::builder().with_url(format!("http://127.0.0.1:{port}")).build().context("Creating Agent")?,
-            test_canister_id: Principal::from_text(test_canister_id)
-                .context("Parsing principal")?,
-            call_canister_id: Principal::from_text(call_canister_id)
-                .context("Parsing principal")?,
-            // cargo_manifest_dir: cargo_manifest_dir.to_path_buf(),
-            workspace_dir: workspace_dir,
-        };
-        res.agent.fetch_root_key().await.context("Fetching root keys.")?; // DON'T USE this on mainnet
+        let agent = Agent::builder().with_url(format!("http://127.0.0.1:{port}")).build().context("Creating Agent")?;
+        agent.fetch_root_key().await.context("Fetching root keys.")?; // DON'T USE this on mainnet
 
-        let toml_path = res.dir.path().join("config.toml");
+        let toml_path = base.dir.path().join("config.toml");
         let toml = read_to_string(&toml_path).context("Reading config.")?;
         let mut doc = toml.parse::<DocumentMut>().context("Invalid TOML")?;
-        doc["callback"]["canister"] = value(res.call_canister_id.to_string());
+        doc["callback"]["canister"] = value(call_canister_id.to_string());
         write(&toml_path, doc.to_string()).context("Writing modified config.")?;
 
-        Ok(res)
+        Ok(Self {
+            base,
+            test_canister_id: Principal::from_text(test_canister_id)
+                .context("Parsing principal")?,
+            // call_canister_id: Principal::from_text(call_canister_id)
+            //     .context("Parsing principal")?,
+            agent,
+        })
     }
 }
 
-async fn test_calls(test: &Test) -> Result<(), Box<dyn std::error::Error>> {
+impl Drop for OurDFX {
+    fn drop(&mut self) {
+        run_successful_command(&mut Command::new(
+            "/root/.local/share/dfx/bin/dfx" // TODO: Split path.
+        ).args(["stop"]).current_dir(self.base.dir.path()))
+            .context("Stopping DFX").expect("can't stop DFX");
+    }
+}
+
+async fn test_calls(test: &OurDFX) -> Result<(), Box<dyn std::error::Error>> {
     let res =
         test.agent.update(&test.test_canister_id, "test").with_arg(Encode!(&()).unwrap())
             .call_and_wait().await.context("Call to IC.")?;
@@ -88,13 +111,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cargo_manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
     let tmpl_dir = cargo_manifest_dir.join("tmpl");
 
-    let test = Test::new(&tmpl_dir).await?;
+    let test = OurDFX::new(&tmpl_dir).await?;
     let _test_http = TemporaryChild::spawn(&mut Command::new(
-        test.workspace_dir.join("target").join("debug").join("test-server")
+        test.base.workspace_dir.join("target").join("debug").join("test-server")
     ), Capture { stdout: None, stderr: None }).context("Running test HTTPS server")?;
     let _proxy = TemporaryChild::spawn(&mut Command::new(
-        test.workspace_dir.join("target").join("debug").join("joining-proxy")
-    ).current_dir(test.dir.path()), Capture { stdout: None, stderr: None }).context("Running Joining Proxy")?;
+        test.base.workspace_dir.join("target").join("debug").join("joining-proxy")
+    ).current_dir(test.base.dir.path()), Capture { stdout: None, stderr: None }).context("Running Joining Proxy")?;
     sleep(Duration::from_millis(1000)).await; // Wait till daemons start.
     test_calls(&test).await?;
     // TODO
