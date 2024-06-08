c171a7bff87bb74904d4db5a595a353d71b32ba6
Victor Porton <porton.victor@gmail.com>
2024-06-08 20:05:09 +0300

    removed a TODO comment

--- proxy/src/cache/cache.rs
+++ proxy/src/cache/cache.rs
@@ -12,6 +12,4 @@ pub trait Cache<K, V> {
 
     // async fn put(&mut self, key: K, value: V) -> MyResult<()>;
 }
-
-// TODO: Avoid copying vectors.
 // pub type BinaryCache = dyn Cache<Vec<u8>, Vec<u8>>;
